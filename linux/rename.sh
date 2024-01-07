8c2a362e135ba381dac5a4a14306e0c86a02e21e
codding-nepale <79444135+codding-nepale@users.noreply.github.com>
2024-01-07 11:39:37 +0100

    Create rename.sh

---
+++ linux/rename.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "Welcome to the interactive file renaming script by codding-nepal."
+echo
+
+read -p "Enter the source folder path (press Enter to use the current folder): " sourceFolder
+sourceFolder=${sourceFolder:-..}
+
+read -p "Enter the file extensions you want to rename (separated by a space, e.g., js mjs): " fileExtensions
+if [ -z "$fileExtensions" ]; then
+    echo "No extensions specified. The script will exit."
+    read -p "Press Enter to continue..."
+    exit 1
+fi
+
+read -p "Enter folders to exclude (separated by a space, press Enter to skip): " excludeFolders
+
+echo
+echo "Renaming in progress..."
+
+for extension in $fileExtensions; do
+    find "$sourceFolder" -type f -name "*.$extension" | while read -r file; do
+        filename=$(basename "$file")
+        dirname=$(dirname "$file")
+        newName="$dirname/${filename%.*}.ts"
+
+        excludeFolder=false
+        if [ -n "$excludeFolders" ]; then
+            IFS=' ' read -ra folders <<< "$excludeFolders"
+            for folder in "${folders[@]}"; do
+                if [[ "$dirname" == *"$folder"* ]]; then
+                    excludeFolder=true
+                    break
+                fi
+            done
+        fi
+
+        if [ "$excludeFolder" != true ]; then
+            echo "Renaming \"$file\" to \"$newName\""
+            mv "$file" "$newName"
+        fi
+    done
+done
+
+echo
+echo "Renaming complete."
+read -p "Press Enter to continue..."
